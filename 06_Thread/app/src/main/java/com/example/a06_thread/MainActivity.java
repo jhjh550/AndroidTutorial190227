01d219e3a265a2fa0fa9377d24b24f9e61ea957e
Your Name <you@example.com>
2019-02-27 17:23:12 +0900

    thread

--- 06_Thread/app/src/main/java/com/example/a06_thread/MainActivity.java
+++ 06_Thread/app/src/main/java/com/example/a06_thread/MainActivity.java
@@ -2,11 +2,26 @@ package com.example.a06_thread;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    class MyThread extends Thread{
+        @Override
+        public void run() {
+            for(int i=0; i<100; i++){
+                Log.d("thread", "count : "+i);
+                btnStart.setText("count : "+i);
+                try {
+                    sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
     Button btnStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +33,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-
+        MyThread th = new MyThread();
+        th.start();
     }
 }
