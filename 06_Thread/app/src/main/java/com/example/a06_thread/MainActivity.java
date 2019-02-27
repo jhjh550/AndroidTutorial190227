c986883da26729aa1817b1c71a22161dcedb4774
Your Name <you@example.com>
2019-02-27 17:41:11 +0900

    thread

--- 06_Thread/app/src/main/java/com/example/a06_thread/MainActivity.java
+++ 06_Thread/app/src/main/java/com/example/a06_thread/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.a06_thread;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,12 +10,24 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final int MY_COUNT = 1;
+    Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            if(msg.what == MY_COUNT){
+                btnStart.setText("count "+msg.arg1);
+            }
+        }
+    };
     class MyThread extends Thread{
         @Override
         public void run() {
             for(int i=0; i<100; i++){
                 Log.d("thread", "count : "+i);
-                btnStart.setText("count : "+i);
+                Message msg = new Message();
+                msg.what = MY_COUNT;
+                msg.arg1 = i;
+                mHandler.sendMessage(msg);
                 try {
                     sleep(100);
                 } catch (InterruptedException e) {
