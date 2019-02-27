95a4944cd47850692011af2401ff0251790667a3
Your Name <you@example.com>
2019-02-27 11:51:27 +0900

    widget

--- 01_Widget/app/src/main/java/com/example/a01_widget/MainActivity.java
+++ 01_Widget/app/src/main/java/com/example/a01_widget/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.a01_widget;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.textView);
+
+        Button btnHello = findViewById(R.id.btnHello);
+        btnHello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Button Clicked!");
+            }
+        });
     }
 }
