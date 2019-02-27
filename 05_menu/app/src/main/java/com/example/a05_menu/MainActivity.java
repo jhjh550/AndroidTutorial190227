4d144d7c84fe8e5c8959c3fce50977d4085b8f81
Your Name <you@example.com>
2019-02-27 17:01:19 +0900

    menu

--- 05_menu/app/src/main/java/com/example/a05_menu/MainActivity.java
+++ 05_menu/app/src/main/java/com/example/a05_menu/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.a05_menu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item_menu_1:
+                Log.d("menu", "menu 1");
+                break;
+            case R.id.item_menu_2:
+                Log.d("menu", "menu 2");
+                break;
+        }
+        return true;
     }
 }
