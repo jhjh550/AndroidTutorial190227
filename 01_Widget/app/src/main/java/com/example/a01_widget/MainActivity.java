a8cad69ccbc15327578a23cca89c6c37bbccf07e
Your Name <you@example.com>
2019-02-27 12:22:59 +0900

    widget

--- 01_Widget/app/src/main/java/com/example/a01_widget/MainActivity.java
+++ 01_Widget/app/src/main/java/com/example/a01_widget/MainActivity.java
@@ -2,29 +2,54 @@ package com.example.a01_widget;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    EditText myEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         final TextView textView = findViewById(R.id.textView);
+
+        myEditText = findViewById(R.id.myEditText);
+        myEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                textView.setText(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
 
         Button btnHello = findViewById(R.id.btnHello);
         btnHello.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setText("Button Clicked!");
+                String str = myEditText.getText().toString();
+                textView.setText(str);
+                myEditText.setText("");
             }
         });
 
@@ -34,6 +59,21 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 Toast.makeText(MainActivity.this, "checked", Toast.LENGTH_SHORT).show();
                 Log.d(TAG, "check box "+isChecked);
+            }
+        });
+
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.radio1:
+                        break;
+                    case R.id.radio2:
+                        break;
+                    case R.id.radio3:
+                        break;
+                }
             }
         });
 
