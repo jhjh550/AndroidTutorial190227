272de7d2442dac421d3d18fcf394dc0abe43008e
Your Name <you@example.com>
2019-02-27 14:54:58 +0900

    activity

--- 02_activity/app/src/main/java/com/example/a02_activity/MainActivity.java
+++ 02_activity/app/src/main/java/com/example/a02_activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.a02_activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyActivity.class);
+                intent.putExtra("myValue", "hello world");
+                intent.putExtra("intValue", 100);
+                startActivity(intent);
+            }
+        });
     }
 }
--- 02_activity/app/src/main/java/com/example/a02_activity/MyActivity.java
+++ 02_activity/app/src/main/java/com/example/a02_activity/MyActivity.java
@@ -1,7 +1,9 @@
 package com.example.a02_activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("myValue");
+        int intValue = intent.getIntExtra("intValue", 0);
+
+        Toast.makeText(this, str+intValue, Toast.LENGTH_SHORT).show();
     }
 }
