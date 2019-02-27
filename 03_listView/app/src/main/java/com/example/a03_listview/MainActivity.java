5114b6bf7917c98d466684e7b0c8c695a9c10770
Your Name <you@example.com>
2019-02-27 15:38:48 +0900

    listView

--- 03_listView/app/src/main/java/com/example/a03_listview/MainActivity.java
+++ 03_listView/app/src/main/java/com/example/a03_listview/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.a03_listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,5 +28,11 @@ public class MainActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_1,
                 list);
         mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, list[position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
