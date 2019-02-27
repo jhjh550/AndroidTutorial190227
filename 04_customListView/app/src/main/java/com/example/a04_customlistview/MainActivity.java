a1a571e872c3dd3fa5b115c4b68711569a86f145
Your Name <you@example.com>
2019-02-27 15:49:18 +0900

    custom list view

--- 04_customListView/app/src/main/java/com/example/a04_customlistview/MainActivity.java
+++ 04_customListView/app/src/main/java/com/example/a04_customlistview/MainActivity.java
@@ -3,11 +3,34 @@ package com.example.a04_customlistview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    class MyData{
+        String title;
+        String desc;
+        int imgIcon;
+
+        public MyData(String title, String desc, int imgIcon) {
+            this.title = title;
+            this.desc = desc;
+            this.imgIcon = imgIcon;
+        }
+    }
+
+    ArrayList<MyData> list = new ArrayList<>();
+    private void setMyData(){
+        for(int i=0; i<100; i++){
+            int icon = i%2==0 ? R.mipmap.ic_launcher : R.mipmap.ic_launcher_round;
+            MyData myData = new MyData("title"+i, "desc"+i, icon);
+            list.add(myData);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setMyData();
     }
 }
