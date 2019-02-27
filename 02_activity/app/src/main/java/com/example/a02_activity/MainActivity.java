8a87d5e4a6a826d13ef496b2b5e21acecd35bc7d
Your Name <you@example.com>
2019-02-27 15:10:49 +0900

    activity

--- 02_activity/app/src/main/java/com/example/a02_activity/MainActivity.java
+++ 02_activity/app/src/main/java/com/example/a02_activity/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.a02_activity;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final int MY_REQ_CODE = 100;
@@ -25,5 +27,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == MY_REQ_CODE && resultCode == RESULT_OK){
+            String str = data.getStringExtra("myResult");
+            Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+        }
+    }
 }
