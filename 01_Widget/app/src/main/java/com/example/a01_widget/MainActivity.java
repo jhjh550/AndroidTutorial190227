a3b2e0e49b40400c212d07477d34b481a605e1f2
Your Name <you@example.com>
2019-02-27 12:04:01 +0900

    widget

--- 01_Widget/app/src/main/java/com/example/a01_widget/MainActivity.java
+++ 01_Widget/app/src/main/java/com/example/a01_widget/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.a01_widget;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +27,15 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText("Button Clicked!");
             }
         });
+
+        final CheckBox checkBox2 = findViewById(R.id.checkBox2);
+        checkBox2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(MainActivity.this, "checked", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "check box "+isChecked);
+            }
+        });
+
     }
 }
