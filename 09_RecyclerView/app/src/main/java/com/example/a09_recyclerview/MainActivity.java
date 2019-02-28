3e4d8c7f36a5e9975fd92eaae238355002c6f5a4
Your Name <you@example.com>
2019-02-28 13:56:40 +0900

    recycler view

--- 09_RecyclerView/app/src/main/java/com/example/a09_recyclerview/MainActivity.java
+++ 09_RecyclerView/app/src/main/java/com/example/a09_recyclerview/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.a09_recyclerview;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] list = {"hello", "world", "oracle", "java",
+            "hello", "world", "oracle", "java",
+            "hello", "world", "oracle", "java",
+            "hello", "world", "oracle", "java"
+    };
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        MyAdapter adapter = new MyAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView textView;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView);
+        }
+    }
+
+    class MyAdapter extends RecyclerView.Adapter<MyViewHolder>{
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            LayoutInflater inf = LayoutInflater.from(MainActivity.this);
+            View v = inf.inflate(R.layout.item_view, viewGroup, false);
+            return new MyViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+            myViewHolder.textView.setText(list[i]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return list.length;
+        }
     }
 }
