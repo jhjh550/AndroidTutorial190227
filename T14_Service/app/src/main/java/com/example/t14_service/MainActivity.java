e6d21fe4fb38c1a5e318724e80c4dd3406e3be55
Your Name <you@example.com>
2019-03-04 17:40:01 +0900

    service

--- T14_Service/app/src/main/java/com/example/t14_service/MainActivity.java
+++ T14_Service/app/src/main/java/com/example/t14_service/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.t14_service;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnStart).setOnClickListener(this);
+        findViewById(R.id.btnStop).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btnStart){
+            Intent intent = new Intent(this, MyService.class);
+            startService(intent);
+        }else{
+            Intent intent = new Intent(this, MyService.class);
+            stopService(intent);
+        }
     }
 }
--- T14_Service/app/src/main/java/com/example/t14_service/MyService.java
+++ T14_Service/app/src/main/java/com/example/t14_service/MyService.java
@@ -3,9 +3,26 @@ package com.example.t14_service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MyService extends Service {
-    public MyService() {
+    private static final String TAG = "MyService";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG, "onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
     }
 
     @Override
