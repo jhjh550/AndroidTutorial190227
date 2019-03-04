e1d8d05a0b6072d85832a4e333533be9bd5dc5b7
Your Name <you@example.com>
2019-03-04 14:19:30 +0900

    sqlite

--- T12_SQLite/app/src/main/java/com/example/t12_sqlite/TestDBHandler.java
+++ T12_SQLite/app/src/main/java/com/example/t12_sqlite/TestDBHandler.java
@@ -1,11 +1,28 @@
 package com.example.t12_sqlite;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
 public class TestDBHandler {
     TestSQLiteOpenHelper helper;
 
     public TestDBHandler(Context context) {
         helper = new TestSQLiteOpenHelper(context, "people.db", null, 1);
+    }
+
+    public void insert(String name, int age, String address){
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("name", name);
+        values.put("age", age);
+        values.put("address", address);
+        db.insert("student", null, values);
+    }
+
+    public void delete(String name){
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.delete("student", "name = ?",
+                new String[]{name});
     }
 }
