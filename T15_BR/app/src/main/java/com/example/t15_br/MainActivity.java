1b096320733f590c636aa84d68d44184990d7e7d
Your Name <you@example.com>
2019-03-05 10:30:25 +0900

    BR

--- T15_BR/app/src/main/java/com/example/t15_br/MainActivity.java
+++ T15_BR/app/src/main/java/com/example/t15_br/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.t15_br;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if(action.equals(Intent.ACTION_BATTERY_CHANGED)) {
+                int level = intent.getIntExtra("level", 0);
+                Log.d("Level", "battery level : " + level);
+                Toast.makeText(context, "battery level : " + level, Toast.LENGTH_SHORT).show();
+            }else if(action.equals(Intent.ACTION_BATTERY_LOW)){
+
+            }else if(action.equals("abcdefg")){
+
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btnSend).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("abcdefg");
+                sendBroadcast(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        filter.addAction(Intent.ACTION_BATTERY_LOW);
+        filter.addAction("abcdefg");
+        registerReceiver(receiver, filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(receiver);
     }
 }
